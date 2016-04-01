5ae92e01f20a7b93677ae689ce2e75d026f96b97
Your Name <wcgxiaoq@qq.com>
2016-04-01 18:28:45 +0800

    增加微信分享功能

--- app/src/main/java/com/edison/esearch/activity/MainActivity.java
+++ app/src/main/java/com/edison/esearch/activity/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import com.edison.eaction.ActionCallbackListener;
 import com.edison.esearch.R;
+import com.edison.esearch.utils.ShareUtils;
 import com.edison.model.PhoneInfo;
 
 /**
@@ -17,7 +18,7 @@ public class MainActivity extends EBaseActivity {
 
 
     private TextView tv;
-    private Button btn ;
+    private Button btn;
     private String abc = "";
 
     @Override
@@ -28,24 +29,30 @@ public class MainActivity extends EBaseActivity {
 
     }
 
-    public void initView(){
+    public void initView() {
         tv = (TextView) findViewById(R.id.text_weather);
         btn = (Button) findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getinfo();
+//                getinfo();
+                ShareUtils.share(MainActivity.this, "testshare", "https:" +
+                        "//ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd" +
+                        "_logo1_31bdc765.png", "https://www.baidu.com/", "分享测试内容");
             }
         });
     }
 
-    public void getinfo(){
+
+    public void getinfo() {
 
         this.appAction.phoneInfo("13681885767", new ActionCallbackListener<PhoneInfo>() {
             @Override
             public void onSuccess(PhoneInfo data) {
-                Log.i("归属地","qqqqq"+data.getProvince());
-                tv.setText("AAA"+data.getProvince());
+                Log.i("归属地", "qqqqq" + data.getProvince());
+                tv.setText("AAA" + data.getProvince());
+
+
             }
 
             @Override
@@ -55,7 +62,6 @@ public class MainActivity extends EBaseActivity {
         });
 
     }
-
 
 
 }
